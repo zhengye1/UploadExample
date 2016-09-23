fff8a2772d9f46cbc816ff921eda6cb313e75d99
Vincent Zheng <zhengye1@mail.utoronto.ca>
2016-09-23 17:30:26 -0400

    Update the -1 in openshift directory

--- src/main/java/com/uploadExample/controller/UploadController.java
+++ src/main/java/com/uploadExample/controller/UploadController.java
@@ -48,7 +48,7 @@ public class UploadController {
 				String openshiftProperty = System.getenv("OPENSHIFT_DATA_DIR");
 				logger.info("The openshift to Upload is " + openshiftProperty);
 				if (openshiftProperty != null){
-					realPathtoUploads = openshiftProperty.substring(0, openshiftProperty.length())
+					realPathtoUploads = openshiftProperty.substring(0, openshiftProperty.length() - 1)
 							+ uploadsDir;
 				}
 				else{
