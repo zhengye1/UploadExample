846971f4212f84cdb51cf0ebd74a966e63f42e23
Vincent Zheng <zhengye1@mail.utoronto.ca>
2016-09-26 12:41:24 -0400

    Update for openshift

--- src/main/java/com/uploadExample/controller/UploadController.java
+++ src/main/java/com/uploadExample/controller/UploadController.java
@@ -38,6 +38,7 @@ public class UploadController {
 	
 	@RequestMapping(value="/list", method = RequestMethod.GET)
     public String list(Model model){
+		
 		model.addAttribute("users", userService.findAll());
 		return "list";
 	}
