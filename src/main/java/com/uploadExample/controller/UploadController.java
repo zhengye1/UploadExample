6883d4db86c57d7a69c6d05db9fdd822027d1af6
Vincent Zheng <zhengye1@mail.utoronto.ca>
2016-09-26 12:41:57 -0400

    Update

--- src/main/java/com/uploadExample/controller/UploadController.java
+++ src/main/java/com/uploadExample/controller/UploadController.java
@@ -38,7 +38,6 @@ public class UploadController {
 	
 	@RequestMapping(value="/list", method = RequestMethod.GET)
     public String list(Model model){
-		
 		model.addAttribute("users", userService.findAll());
 		return "list";
 	}
